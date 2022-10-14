b899bc0b7a7369351223753742cf9e46874e1327
manvendra <manvendrachauhan535@gmail.com>
2022-10-14 20:29:42 +0530

    node2

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const App = express();
+
+App.get('/', (req, res)=>{
+    res.send('hello Node!');
+})
+App.get('/api/main', (req, res)=>{
+    res.send('Howdy!');
+})
+
+
+App.listen(9000,()=>{
+    console.log('Welcome to my server!');
+})
